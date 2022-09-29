363a0978f4f1f42099f9c0256de791d8cdb08bc2
phinic925 <phinicnyangate480@gmail.com>
2022-09-29 10:08:54 +0100

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,14 @@
 class ItemsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
+
+    private
+
+    def not_found
+        render json: { errors: 'You done fucked up, A-A-ron!! We can\'t find our items!' }, status: :not_found
+    end
 end
