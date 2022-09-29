59be26cb1c8b1bc72bd7ea163b536433eb52ec1f
phinic925 <phinicnyangate480@gmail.com>
2022-09-29 10:08:42 +0100

    commit tree 7f1b9c4ced1c3f38618bca5cd18ab39650e548d1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def not_found
+        render json: { errors: "User not found" }, status: :not_found
+    end
 end
